3965d0d1d024cbfc991f0939de3e670e3cbc466b
saff-it <saff_it@ymail.com>
2022-07-21 15:39:42 +0200

    Milestone 1 - axios call done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,50 @@
 <template>
   <div id="app">
+    <headerFlix @search="" />
     <mainFlix />
-    <headerFlix />
+    
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 import headerFlix from './components/headerFlix.vue'
 import mainFlix from './components/mainFlix.vue'
 
 export default {
+  data: function() {
+    return{
+      dataMovies: [],
+    }
+  },
+
   name: 'App',
   components: {
     mainFlix,
     headerFlix,
-  }
+  },
+
+  methods: {
+    getDataMovies() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=76a0f6b38de7b73cfe6c7dd173889613')
+        .then((result) => {
+          this.dataMovies = result.data.results;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
+
+    
+
+  },
+
+  created() {
+    this.getDataMovies();
+  },
+
+
 }
 </script>
 
