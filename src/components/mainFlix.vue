6019409b125826e7b4db87a019d06c2af10a2d81
saff-it <saff_it@ymail.com>
2022-07-21 17:55:13 +0200

    Milestone 2 - flag substituition first attempt

--- src/components/mainFlix.vue
+++ src/components/mainFlix.vue
@@ -3,7 +3,11 @@
     <ul>
       <li>{{films.title}}</li>
       <li>{{films.original_title}}</li>
-      <li>{{films.original_language}}</li>
+
+      <li v-if="films.original_language === 'en'" > <img src="../assets/img/en.png" alt="uk flag"></li>
+      <li v-else-if="films.original_language === 'it'" > <img src="../assets/img/it.png" alt="it flag"></li>
+      <li v-else>{{films.original_language}}</li>
+
       <li>{{films.vote_average}}</li>
     </ul>
 
@@ -14,6 +18,12 @@
 <script>
 export default {
   props: ['films'],
+
+  flagLang: [
+    'es',
+    'it',
+  ],
+  
 }
 </script>
 
