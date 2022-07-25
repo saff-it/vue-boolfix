6eca542e319b2faadec22fca31ff5c8e4cbffa34
saff-it <saff_it@ymail.com>
2022-07-25 15:32:53 +0200

    Milestone 4 - hover not working

--- src/components/filmCard.vue
+++ src/components/filmCard.vue
@@ -1,23 +1,31 @@
 <template>
 
-    <li>
-        
-        <div class="ms_poster-container">
-          <img :src="`https://image.tmdb.org/t/p/w185/${singleFilm.poster_path}`" :alt="singleFilm.title">
-        </div>
+  <li>
 
-        <h5> <span> Titolo: </span>{{ singleFilm.title }}</h5>
-        <h5> <span> Titolo Originale: </span>{{ singleFilm.original_title }}</h5>
+    <div class="ms_poster-container">
+      <img :src="`https://image.tmdb.org/t/p/w185/${singleFilm.poster_path}`" :alt="singleFilm.title">
+    </div>
 
-        <p>{{singleFilm.overview}}</p>
+    <div class="ms_text-container">
 
-        <div class="ms_flag-container" v-if="singleFilm.original_language === 'en'"> <img src="../assets/img/en.png" alt="uk flag"> </div>
-        <div class="ms_flag-container" v-else-if="singleFilm.original_language === 'it'"> <img src="../assets/img/it.png" alt="it flag"> </div>
-        <div class="ms_flag-container" v-else>{{ singleFilm.original_language }}</div>
+      <h5> <span> Titolo: </span>{{ singleFilm.title }}</h5>
+      <h5> <span> Titolo Originale: </span>{{ singleFilm.original_title }}</h5>
 
-        <i v-for="index in getNumber(singleFilm.vote_average)" :key="index" class="fa-solid fa-star"></i>
+      <p>{{ singleFilm.overview }}</p>
 
-    </li>
+      <div class="ms_flag-container" v-if="singleFilm.original_language === 'en'"> 
+        <img src="../assets/img/en.png" alt="uk flag">
+      </div>
+      <div class="ms_flag-container" v-else-if="singleFilm.original_language === 'it'">
+        <img src="../assets/img/it.png" alt="it flag">
+      </div>
+      <div class="ms_flag-container" v-else>{{ singleFilm.original_language }}</div>
+
+      <i v-for="index in getNumber(singleFilm.vote_average)" :key="index" class="fa-solid fa-star"></i>
+
+    </div>
+
+  </li>
 
 </template>
 
@@ -28,65 +36,73 @@ export default {
   ],
 
   methods: {
-        getNumber(number) {
-            return Math.round(number / 2);
-        }
-    },  
+    getNumber(number) {
+      return Math.round(number / 2);
+    }
+  },
 
 }
 </script>
 
 <style lang="scss" scoped>
-
-li{
+li {
   width: calc(100% / 5 - 20px);
   margin: 0 10px 30px 10px;
   border: 2px solid red;
   background-color: black;
-  
-  div.ms_poster-container{
+
+  div.ms_poster-container {
     width: 100%;
 
-    img{
-    width: 100%;
-    }
-  }
-
-  div.ms_flag-container{
-    width: 12%;
-    display: inline-block;
-    margin: 5px 5px 15px 5px;
-
-
-    img{
+    img {
       width: 100%;
     }
   }
 
-  i{
-    color: orange;
-  }
+  div.ms_flag-container {
+    width: 12%;
+    display: inline-block;
+    margin: 5px 5px 15px 15px;
 
-  h5{
-    font-size: 0.8rem;
-    color: white;
-    margin: 5px 5px 0 5px;
-    font-weight: lighter;
 
-    span{
-    font-size: 0.9rem;
-    font-weight: bold;
-
+    img {
+      width: 100%;
     }
   }
 
-  p{
-    margin: 10px 5px;
-    font-size: 0.8rem;
-    color: rgb(114, 111, 111);
+  .ms_text-container{
+    display: none;
+
+    i {
+      color: orange;
+    }
+
+    h5 {
+      font-size: 0.8rem;
+      color: white;
+      margin: 5px 15px 0 15px;
+      font-weight: lighter;
+
+      span {
+        font-size: 0.9rem;
+        font-weight: bold;
+
+      }
+    }
+
+    p {
+      margin: 10px 15px;
+      font-size: 0.8rem;
+      color: rgb(114, 111, 111);
+      text-align: justify;
+    }
   }
-  
+
+  .ms_text-container:hover{
+    display: block;
+  }
+
+
+
 }
-
-
 </style>
